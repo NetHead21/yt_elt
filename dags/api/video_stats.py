e7d4c597e1c6f1757bcd687db69650074baf1618
nethead21 <junivensaavedra@gmail.com>
2026-04-25 21:51:31 +0800

    Add YoutubeClient class for YouTube Data API interaction

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -5,3 +5,14 @@ import os
 from .data_staging import save_to_json
 
 MAX_RESULTS = 50
+
+
+class YoutubeClient:
+    """Client for interacting with the YouTube Data API v3."""
+
+    def __init__(self, api_key: str):
+        """
+        Args:
+            api_key: YouTube Data API v3 key.
+        """
+        self.api_key = api_key
