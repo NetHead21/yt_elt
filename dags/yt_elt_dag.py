f6b33c2e9cc24230e7c3e8e66d85c854b1669495
nethead21 <junivensaavedra@gmail.com>
2026-04-28 01:00:48 +0800

    Define the yt_elt_dag with daily schedule and get_playlist_id task

--- dags/yt_elt_dag.py
+++ dags/yt_elt_dag.py
@@ -4,3 +4,13 @@ from datetime import datetime
 
 from api.video_stats import YoutubeClient
 from api.data_staging import save_to_json
+
+
+@dag(schedule="@daily", start_date=datetime(2026, 1, 1), catchup=False)
+def yt_elt_dag():
+    @task
+    def get_playlist_id() -> str:
+        client = YoutubeClient(api_key=Variable.get("API_KEY"))
+        channel_handle = Variable.get("CHANNEL_HANDLE")
+        channel_id = client.get_channel_id(channel_handle)
+        return f"UU{channel_id[2:]}"
