b9e127c8980fb74e991f3d253c507ce012e425b8
nethead21 <junivensaavedra@gmail.com>
2026-04-30 00:38:06 +0800

    Add test to verify SQL contains "CREATE TABLE" in TestCreateTable class

--- tests/test_database.py
+++ tests/test_database.py
@@ -166,3 +166,8 @@ class TestCreateTable:
         executed_sql = str(mock_cursor.execute.call_args[0][0])
         for col in COLUMNS:
             assert col in executed_sql
+
+    def test_sql_contains_create_table(self, db, mock_cursor):
+        db.create_table("yt_api", "staging", COLUMNS)
+        executed_sql = str(mock_cursor.execute.call_args[0][0])
+        assert "CREATE TABLE" in executed_sql
