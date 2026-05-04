8177f09ac38bcc2ea770c5bb88e2deb52635709c
nethead21 <junivensaavedra@gmail.com>
2026-05-04 23:57:52 +0800

    Add CORE_TABLE schema definition for yt_api in warehouse_dag.py

--- dags/warehouse_dag.py
+++ dags/warehouse_dag.py
@@ -21,3 +21,18 @@ STAGING_TABLE = {
         "comment_count": "INT",
     },
 }
+
+CORE_TABLE = {
+    "schema_name": "core",
+    "table_name": "yt_api",
+    "columns": {
+        "video_id": "VARCHAR(11) PRIMARY KEY NOT NULL",
+        "title": "TEXT NOT NULL",
+        "published_at": "TIMESTAMP NOT NULL",
+        "duration": "TIME NOT NULL",
+        "video_type": "VARCHAR(10) NOT NULL",
+        "view_count": "INT",
+        "like_count": "INT",
+        "comment_count": "INT",
+    },
+}
